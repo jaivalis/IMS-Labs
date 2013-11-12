b9bec0712aa10c022325c653a3ab1a0972f39aac
TopSteely <tobias.stahl1@gmx.de>
2013-11-12 12:40:21 +0100

    exercise2 changed rgb division to double

--- Assignment1/colorSpace.m
+++ Assignment1/colorSpace.m
@@ -5,7 +5,7 @@ imageSize = size(img);
 width = imageSize(1);
 height = imageSize(2);
 
-redValue = img(:,:,1);
+redValue = img(:, :, 1);
 greenValue = img(:, :, 2);
 blueValue = img(:, :, 3);
 %imshow(redValue);
@@ -15,41 +15,38 @@ opp = zeros(width,height,3);
 if (colorSpace == 1),
     for x=1:width,
         for y=1:height,
-            opp1(x,y) = (redValue(x, y) - greenValue(x, y) / sqrt(2));
-            opp2(x,y) = (redValue(x, y) + greenValue(x, y) - 2 * blueValue(x, y)/ sqrt(6));
-            opp3(x,y) = (redValue(x, y) + greenValue(x, y) + blueValue(x, y)/ sqrt(3));
+            opp1(x, y) = ((redValue(x, y) - greenValue(x, y)) / sqrt(2));
+            opp2(x, y) = ((redValue(x, y) + greenValue(x, y) - 2 * blueValue(x, y))/ sqrt(6));
+            opp3(x, y) = ((redValue(x, y) + greenValue(x, y) + blueValue(x, y))/ sqrt(3));
         end
     end
-    figO = figure('name','Opponent color space');
+    figO = figure('name', 'Opponent color space');
     set(figO, 'name','Opponent color space');
-    subplot(2,2,1);
+    subplot(2, 2, 1);
     imshow(img);
-    subplot(2,2,2);
+    subplot(2, 2, 2);
     imshow(opp1);
-    subplot(2,2,3);
+    subplot(2, 2, 3);
     imshow(opp2);
-    subplot(2,2,4);
+    subplot(2, 2, 4);
     imshow(opp3);
 end
 
 %rgb color space
 rgb = zeros(width,height,3);
 if (colorSpace == 2),
-    for x=1:width,
-        for y=1:height,
-            rgb1(x,y) = redValue(x, y) / redValue(x, y) + greenValue(x, y) + blueValue(x, y);
-            rgb2(x,y) = greenValue(x, y) / redValue(x, y) + greenValue(x, y) + blueValue(x, y);
-            rgb3(x,y) = blueValue(x, y) / redValue(x, y) + greenValue(x, y) + blueValue(x, y);
-        end
-    end
-    figure('name','RGB color space');;
-    subplot(2,2,1);
+    norm = img(:,:,1) + img(:,:,2) + img(:,:,3);
+    rgb1 = double(img(:,:,1)./norm);
+    rgb2 = double(img(:,:,2)./norm);
+    rgb3 = double(img(:,:,3)./norm);
+    figure('name', 'RGB color space');
+    subplot(2, 2, 1);
     imshow(img);
-    subplot(2,2,2);
+    subplot(2, 2, 2);
     imshow(rgb1);
-    subplot(2,2,3);
+    subplot(2, 2, 3);
     imshow(rgb2);
-    subplot(2,2,4);
+    subplot(2, 2, 4);
     imshow(rgb3);
 end
 
@@ -57,13 +54,13 @@ end
 %hsv color space
 if (colorSpace == 3),
     hsv_image = rgb2hsv(img);
-    figure('name','HSV color space');;
-    subplot(2,2,1);
-    imshow(img);
-    subplot(2,2,2);
-    imshow(hsv_image(:,:,1));
-    subplot(2,2,3);
-    imshow(hsv_image(:,:,2));
-    subplot(2,2,4);
-    imshow(hsv_image(:,:,3));
+    figure('name', 'HSV color space');
+    subplot(2, 2, 1);
+    imshow(hsv_image);
+    subplot(2, 2, 2);
+    imshow(hsv_image(:, :, 1));
+    subplot(2, 2, 3);
+    imshow(hsv_image(:, :, 2));
+    subplot(2, 2, 4);
+    imshow(hsv_image(:, :, 3));
 end
