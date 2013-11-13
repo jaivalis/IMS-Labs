2580fd36a7c121af658e68c3512e4d0226bcf51d
jaivalis <johnaivalis@gmail.com>
2013-11-13 15:24:05 +0100

    final ex1

--- Assignment1/photometricStereo.m
+++ Assignment1/photometricStereo.m
@@ -1,126 +1,104 @@
 function photometricStereo()
-img_size = 512;
-k = 512;
-% Obtain many images in a fixed view under different illuminants
-img1 = imread('sphere1.png'); % S center        [256 256]
-img2 = imread('sphere2.png'); % S lower right   [512 512]
-img3 = imread('sphere3.png'); % S lower left    [1 512]
-img4 = imread('sphere4.png'); % S upper right   [512 1]
-img5 = imread('sphere5.png'); % S upper left    [1 1]
+  global img_size;
+  img_size = 512;
+  k = sqrt(1/3);
+  % Obtain many images in a fixed view under different illuminants
+  img1 = im2single(imread('sphere1.png')); % S center        [256 256]
+  img2 = im2single(imread('sphere2.png')); % S lower right   [512 512]
+  img3 = im2single(imread('sphere3.png')); % S lower left    [1 512]
+  img4 = im2single(imread('sphere4.png')); % S upper right   [512 1]
+  img5 = im2single(imread('sphere5.png')); % S upper left    [1 1]
 
-S1 = zeros(512, 512, 3);
-S2 = zeros(512, 512, 3);
-S3 = zeros(512, 512, 3);
-S4 = zeros(512, 512, 3);
-S5 = zeros(512, 512, 3);
+  S1_pos = [ 0  0  1];
+  S2_pos = [ k  k  k];
+  S3_pos = [-k  k  k];
+  S4_pos = [ k -k  k];
+  S5_pos = [-k -k  k];
 
-S1_pos = [0  0  -1];
-S2_pos = [1  1  -1];
-S3_pos = [-1 1  -1];
-S4_pos = [1  -1 -1];
-S5_pos = [-1 -1 -1];
+  % Determine the matrix V from source and camera information
+  V = [k * S1_pos; k * S2_pos; k * S3_pos; k * S4_pos; k * S5_pos;];
 
-% Determine the matrix V from source and camera information
-V = [k * S1_pos; k * S2_pos; k * S3_pos; k * S4_pos; k * S5_pos;];
+  I = zeros(img_size, img_size, 5);
 
-I = zeros(img_size, img_size, 5);
+  % Create arrays for albedo, normal (3 components),
+  albedo = zeros(512,512);
+  normal = zeros(512, 512, 3);
+  % p (measured value of ∂f/∂x )
+  p = zeros(512,512);
+  % q (measured value of ∂f/∂y )
+  q = zeros(512,512);
 
-% g = zeros(512, 512, 3);
+  % For each point in the image array
+  for i = 1:img_size,
+    for j = 1:img_size,
+      % Stack image values into a vector I
+      I(i, j, :) = [img1(i,j); img2(i,j); img3(i,j); img4(i,j); img5(i,j);];
 
-% Create arrays for albedo, normal (3 components),
-albedo = zeros(512,512);
-normal = zeros(512, 512, 3);
-% p (measured value of ∂f/∂x )
-p = zeros(512,512);
-% q (measured value of ∂f/∂y )
-q = zeros(512,512);
+      % Construct the diagonal matrix T
+      II =  reshape(I(i,j,:), 1, 5);
+      T = diag(II);
 
-opts.RECT = true;
-% For each point in the image array
-for i = 1:img_size,
-  for j = 1:img_size,
-    S1(i, j, :) = [(i - S1_pos(1)) (j - S1_pos(2)) (100)];
-    S2(i, j, :) = [(i - S2_pos(1)) (j - S2_pos(2)) (100)];
-    S3(i, j, :) = [(i - S3_pos(1)) (j - S3_pos(2)) (100)];
-    S4(i, j, :) = [(i - S4_pos(1)) (j - S4_pos(2)) (100)];
-    S5(i, j, :) = [(i - S5_pos(1)) (j - S5_pos(2)) (100)];
+      % Solve TVg = TI to obtain g for this point
+      A = T*V;
+      if A == zeros(5,3),   % avoiding linsolve warnings
+        g = zeros(1, 3);
+      else
+        B = T*II.';
+        g = linsolve(A, B);
+      end
+      
+      % albedo at this point is norm(g)
+      normG = norm(g);
+      albedo(i, j) = normG;
 
-    % Stack image values into a vector I
-    I(i, j, :) = [img1(i,j); img2(i,j); img3(i,j); img4(i,j); img5(i,j);];
+      if normG == 0,  normG = 1;  end
+      
+      % normal at this point is g / norm(g)
+      normal(i, j, :) = g ./ normG;
 
-    % Construct the diagonal matrix T
-    II =  reshape(I(i,j,:), 5, 1);
-    T = diag(II);
-
-    % Solve TVg = TI to obtain g for this point
-    TV = T*V;
-    if TV == zeros(5,3),  linsolv = zeros(3,1);
-    else                  linsolv = linsolve(TV, T*II, opts);
-                        % linsolv = (T*II')\(T*V);
+      if normal(i, j, 3) == 0, % avoiding division by 0
+        normal(i, j, 3) = 0.00001;
+      end
+      % p at this point is N1 / N3
+      p(i,j) = normal(i, j, 1) / normal(i, j, 3);
+      % q at this point is N2 / N3
+      q(i,j) = normal(i, j, 2) / normal(i, j, 3);
     end
-    g = linsolv;
+  end
+  
+  %reconstruct the surface
+  heightMap = getHeightMap(q, p);
+  figure(1);
+  surf(heightMap(1:25:end, 1:25:end));
 
-    % albedo at this point is norm(g)
-    normG = norm(g);
-    albedo(i,j) = normG;
+  figure(2);
+  imshow(albedo);
 
-    if albedo(i,j) ~= 0,
-        % normal at this point is | g |
-        normal(i,j,:) = g / normG;
-        p(i,j) = normal(i,j,1) / normal(i,j,3);
-        q(i,j) = normal(i,j,2) / normal(i,j,3);
+  stepsize = 25;
+  range = 1:stepsize:img_size;
+  u = normal(range, range, 1);
+  v = normal(range, range, 2);
+  w = normal(range, range, 3);
+
+  figure(3);
+  quiver3(heightMap(range,range),u,v,w);
+end
+
+function map = getHeightMap(Q, P)
+  global img_size;
+  center = ceil(img_size / 2);
+  map = zeros(img_size);
+
+  % center column:
+  for i = 1:center-1
+    map(center, center-i) = map(center, center-i+1) + P(center, center-i);
+    map(center, center+i) = map(center, center+i-1) - P(center, center+i);
+  end
+  % Rest of the heightmap, starting from center column:
+  for i = 1:img_size
+    for j = 1:center-1
+      map(center-j, i) = map(center-j+1, i) + Q(center-j, i);
+      map(center+j, i) = map(center+j-1, i) - Q(center+j, i);
     end
-    % p at this point is N1 / N3
-    %p(i, j) = normal(i, j, 1) / normal(i, j, 3);
-
-    % q at this point is N2 / N3
-    %q(i, j) = normal(i, j, 2) / normal(i, j, 3);
   end
 end
-
-%top left corner of height map is zero
-heightMap1 = zeros(img_size, img_size);
-%for each pixel in the left column of height map
-for i=2 : img_size
-    % height value=previous height value + corresponding q value
-    heightMap1(i, 1) = heightMap1(i-1, 1) + q(i, 1);
-end
-
-%for each row
-for i=1 : img_size
-    %for each element of the row except for leftmost
-   for j = 2 : img_size
-       %height value = previous height value + corresponding p value
-      heightMap1(i,j) = heightMap1(i, j-1) + p(i, j);
-   end   
-end
-
-figure(1);
-mesh(heightMap1);
-
-figure(2);
-imshow(albedo);
-
-stepsize = 15;
-range = 1:stepsize:512;
-u = normal(range, range, 1);
-v = normal(range, range, 2);
-w = normal(range, range, 3);
-
-[x,y] = meshgrid(range, range);
-% z = zeros(ceil(img_size / stepsize), ceil(img_size / stepsize));
-% for i=1:ceil(img_size / stepsize),
-%     for j=1:ceil(img_size / stepsize),
-%         if isnan(heightMap1(i,j)),
-%             z(i,j) = 0;
-%         else
-%             z(i,j) = heightMap1(i,j);
-%         end
-%     end
-% end
-
-figure(3);
-quiver3(heightMap1(range,range),u,v,w);
-
-%figure(4);
-%quiver3(height(range,range), u, v, w);
