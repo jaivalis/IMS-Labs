b072ad6925e256ec5e545ecaf69ea5cad1b560c6
jaivalis <johnaivalis@gmail.com>
2013-11-26 12:28:34 +0100

    changed Harris parameter from greyscale image to image

--- Assignment3/Harris.m
+++ Assignment3/Harris.m
@@ -1,5 +1,5 @@
-% function H = Harris(img)
-% Harris Corner Detector implementation
+function [ H, r, c ] = Harris(img)
+% HARRIS Corner Detector implementation
 %
 % INPUT
 % img: image to detect edges
@@ -8,15 +8,13 @@
 % H - binary corner matrix (1 if corner 0 otherwise)
 % r - corner row vector (contains the x-coordinate of all the corners)
 % c - corner column vector (contains the y-coordinate of all the corners)
-function [ H, r, c ] = Harris(img)
-  original = img / 255;
+
+  grayImg = double(rgb2gray(img));
   k = .04;
   threshold = 0.0000001;
   n = 9; % window size
   
   sigma = 2;  
-%   for sIndex = 1:length(sigmas),
-%     sigma = sigmas(sIndex);
     
   % compute Gaussian & Gaussian derivative
   G  = gaussian(sigma);
@@ -25,8 +23,8 @@ function [ H, r, c ] = Harris(img)
   % compute derivatives of image
   % I_x: the derivative over the x direction
   % I_y: the derivative over the y direction
-  I_x = conv2(G, Gd, img);
-  I_y = conv2(Gd, G, img);
+  I_x = conv2(G, Gd, grayImg);
+  I_y = conv2(Gd, G, grayImg);
 
   % Q matrix
   % A can be obtained by squaring Ix then convolving it with a Gaussian
@@ -34,32 +32,33 @@ function [ H, r, c ] = Harris(img)
   B = conv2(I_x .* I_y, G, 'same');
   C = conv2(I_y .^ 2,   G, 'same');
 
-  % compute det_Q
+  % compute Q matrix components > compute H
   det_Q = (A .* C) - (B .^ 2);
-  % compute trace_Q
   trace_Q = (A .* C);
-
   H = det_Q - k * trace_Q .^ 2;
 
+  % make use of threshold
   H = (H > threshold);
   H = findCorners(H, n, threshold);
   
   % Populate r & c
   H_size = size(H);
-  h_out = original;
+  h_out = img;
   index = 1;
   for x = 1:H_size(1),
     for y = 1:H_size(2),
       if H(x, y) == 1, % edge case
         r(index) = x;
         c(index) = y;
+%         if x - 4 > 1 && y - 4 > 1,
         h_out(x, y) = 1; % add highlight
+%         end
         index = index + 1;
       end
     end
   end
   
-  % Plotting
+  % Plot
   figure; imshow(I_x); title('Partial derivative of I(x, y) w.r.t x');
   figure; imshow(I_y); title('Partial derivative of I(x, y) w.r.t y');
   figure; imshow(h_out); 
--- Assignment3/TestHarris.m
+++ Assignment3/TestHarris.m
@@ -1,7 +1,7 @@
 
 
-img1 = double(rgb2gray(imread('person_toy/00000001.jpg')));
+img1 = imread('person_toy/00000001.jpg');
 [ H, r, c ] = Harris(img1);
 
-% img2 = double(rgb2gray(imread('sphere1.ppm')));
+% img2 = imread('sphere1.ppm');
 % [ H, r, c ] = Harris(img2);
