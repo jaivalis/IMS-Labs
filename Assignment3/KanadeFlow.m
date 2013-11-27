cdcfa6fbf569bb55d4f671ba9e487cef4806d051
TopSteely <tobias.stahl1@gmx.de>
2013-11-27 15:52:19 +0100

    createRegionsFlow, KanadeFlow

---
+++ Assignment3/KanadeFlow.m
@@ -0,0 +1,87 @@
+function [r, c] = KanadeFlow(img1, img2, r, c)
+% KANADE Optical flow estimation for important points
+%
+% INPUT
+% img1: baseline image
+% img2: second image to find optical flow
+% r: important corners of input img1
+% c: 
+%
+% OUTPUT
+% out: original picture with optical flow vectors of important points
+% r: tracked point r in the next picture
+% c: tracked point r in the next picture
+  
+  if size(img1,3) ~= 3,
+    img1Grey = img1;
+    img2Grey = img2;
+  else
+    img1Grey = double(rgb2gray(img1));
+    img2Grey = double(rgb2gray(img2));
+  end
+
+  img1Grey = img1Grey / 255;    
+  img2Grey = img2Grey / 255;
+  img_size = size(img1Grey);
+  region_size = 15;
+
+  % 1. divide picture in non-overlapping regions
+  % region = region_size x region_size;
+  [A1, regions1, ~, ~] = createRegionsFlow(img1Grey, r, c, region_size);
+  [A2, regions2, r, c] = createRegionsFlow(img2Grey, r, c, region_size);
+  regions_size = size(regions1);
+  region_count = regions_size(1);
+  
+  % compute I_t, transpose(A)
+  I_t = regions2 - regions1;
+  A_t = permute(A1, [1 3 2]);
+  b_temp = - I_t;
+  
+  % reshape b
+  b = zeros(region_count, region_size ^ 2, 1);
+  for i = 1 : region_count,
+      index = 1;
+      for j = 1 : region_size,
+          for k = 1 : region_size,
+              b(i, index,1) = b_temp(i, j, k);
+              index = index + 1;
+          end
+      end
+  end
+
+  % compute v according to equation 20
+    inverse = zeros(region_count, 2, 2);
+    temp1 = zeros(region_count, 2, 2);
+%     temp = zeros(region_count ^ 2, region_size ^ 2, 2);
+    V = zeros(region_count, 2); % wrong shape in this case
+    for i=1:region_count,
+        first = reshape(A_t(i, :, :), 2, region_size ^ 2);
+        second = reshape(A1(i, :, :),region_size ^ 2,2);
+        temp1(i, :, :) = first * second;
+        inverse(i, :, :) = pinv(reshape(temp1(i, :, :), 2, 2));
+        temp(i, :, :) = reshape(inverse(i, :, :), 2, 2) *...
+            reshape(A_t(i, :, :), 2, region_size ^ 2);
+        
+        V(i, :, :) = reshape(temp(i, :, :), 2, region_size ^ 2) *...
+            reshape(b(i, :, :), region_size ^ 2,1);
+    end
+    
+    % reshape v
+    % test if necessary
+%     V = zeros(region_count(1), region_count(2), 2);
+%     index = 1;
+%     for i = 1 : region_count(1),
+%         for j = 1 : region_count(2),
+%             V(i, j, :) = v(index, :);
+%             index = index + 1;
+%         end
+%     end
+    
+    figure;
+    imshow(img1Grey);
+    hold on
+    quiver(r(:), c(:), V(:, 1), V(:, 2));
+    hold off
+    set(gca,'YDir','reverse');
+      
+end
---
+++ Assignment3/createRegionsFlow.m
@@ -0,0 +1,111 @@
+function [A, regions, r_rel, c_rel] = createRegionsFlow(img, r, c, region_size)
+% GETRELEVANTREGIONS 
+%
+% INPUT
+% img1: baseline image
+% img2: second image to find optical flow
+% r: important corners of input img1
+% c: 
+%
+% OUTPUT
+% out: original picture with optical flow vectors of important points
+% r: tracked point r in the next picture
+% c: tracked point r in the next picture
+  img_size = size(img);
+  sigma = 2;
+  % compute Gaussian & Gaussian derivative
+  G  = gaussian(sigma);
+  Gd = gaussianDer(G, sigma);
+  % compute gradient of the whole image
+  Image_x = conv2(G, Gd, img);
+  Image_x = Image_x(sigma:img_size(1) + sigma, sigma:img_size(2) + sigma);
+  Image_y = conv2(Gd, G, img);
+  Image_y = Image_y(sigma:img_size(1) + sigma, sigma:img_size(2) + sigma);
+
+  index = 1;
+  regions = zeros(length(r), region_size, region_size);
+  r_rel = zeros(1, length(r));
+  c_rel = zeros(1, length(c));
+  I_x = zeros(length(r), region_size, region_size);
+  I_y = zeros(length(r), region_size, region_size);
+ 
+  for i=1:length(r),
+    xCenter = r(i);
+    yCenter = c(i);
+    
+    xUpLeft = xCenter - floor(region_size/2);
+    xUpRight = xCenter + floor(region_size/2);
+    yUpLeft = yCenter - floor(region_size/2);
+    yDownLeft = yCenter + floor(region_size/2);
+    
+    % Edges are rendered unimportant
+    if xUpLeft < 1 || yUpLeft < 1 || ... 
+      xUpRight > img_size(1) || yDownLeft > img_size(2),
+      continue;
+    else
+      % coordiates are valid
+      r_rel(index) = xCenter;
+      c_rel(index) = yCenter;
+    end
+    
+    region = img(xUpLeft:xUpRight, yUpLeft:yDownLeft);
+    Ixregion = Image_x(xUpLeft:xUpRight, yUpLeft:yDownLeft);
+    Iyregion = Image_y(xUpLeft:xUpRight, yUpLeft:yDownLeft);
+    
+    regions(index, :, :) = region;
+    I_x(index, :, :) = Ixregion;
+    I_y(index, :, :) = Iyregion;
+    index = index + 1;
+  end
+  
+  region_count = index - 1;
+    
+    % compute A
+  A = zeros(region_count, region_size ^ 2, 2);
+    
+  for i = 1 : region_count,
+    index = 1;
+    for j = 1 : region_size,
+      for k = 1 : region_size,
+        A(i, index,1) = I_x(i, j, k);
+        A(i, index,2) = I_y(i, j, k);
+        index = index + 1;
+      end
+    end
+  end
+  
+  % filter out edges
+  regions = regions(1:region_count, :, :);
+  r_rel = r_rel(1:region_count);
+  c_rel = c_rel(1:region_count);
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    
