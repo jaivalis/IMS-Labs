06e740fe512ee8db07e0981c3dbaf931a2f61e0a
jaivalis <johnaivalis@gmail.com>
2013-11-27 15:02:55 +0100

    bugfix in findCorners

--- Assignment3/TestHarris.m
+++ Assignment3/TestHarris.m
@@ -1,10 +1,10 @@
 
 
-% img1 = imread('person_toy/00000001.jpg');
-% [ H, r, c ] = Harris(img1, 0);
+img1 = imread('person_toy/00000001.jpg');
+[ H, r, c ] = Harris(img1, 0);
 
 % img2 = imread('sphere1.ppm');
 % [ H, r, c ] = Harris(img2, 0);
 
-img2 = imread('synth1.pgm');
-[ H, r, c ] = Harris(img2, 0);
+% img2 = imread('synth1.pgm');
+% [ H, r, c ] = Harris(img2, 0);
--- Assignment3/findCorners.m
+++ Assignment3/findCorners.m
@@ -5,34 +5,16 @@ function H = findCorners( h, N, threshold )
   H = zeros(h_size);
   
   n = floor(N/2);
+  
+%   H = (h > threshold);
   for x = 1:h_size(1),
     for y = 1:h_size(2),
-      xUpLeft    = x-n;  yUpLeft    = x-n;
-      xDownRight = x+n;  yDownRight = x+n;
-      
-      % Sanitize window borders
-      if xUpLeft < 1,   xUpLeft = 1;   end
-      if yUpLeft < 1,   yUpLeft = 1;   end
-      if xDownRight > h_size(2), xDownRight = h_size(2);  end
-      if yDownRight > h_size(2), yDownRight = h_size(2);  end
-      
-      window = h(xUpLeft:yUpLeft, xDownRight:yDownRight);
-      window_size = size(window);
-      flag = 1; % Assume it is an edge, flag = 1;
-      for xx = 1:window_size(1),
-        for yy = 1:window_size(2),
-          if h(x,y) < window(xx, yy),
-            flag = 0; % not an edge
-            break;
-          end
-        end
-        if flag == 0,
-          break % check next pixel
-        end
+      if h(x, y) > threshold,
+        H(x, y) = h(x, y);
+      else
+        H(x, y) = 0;
       end
-%       imregionalmax()
-      H(x, y) = flag;
     end
   end
-  H = ~H; % Negate
+  H = imregionalmax(H);
 end
