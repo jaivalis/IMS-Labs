e20465fd5ae20224f3b27505706852db45f084a7
TopSteely <tobias.stahl1@gmx.de>
2013-11-29 21:58:13 +0100

    sort

--- Assignment4/src/countInliers.m
+++ Assignment4/src/countInliers.m
@@ -15,14 +15,16 @@ sizeT = size( T );
 inliers = 0;
 
 for i = 1:T,
-  % sort matches, so that points of img1 are in order
   % [X;Y;S;TH], where X,Y is the (fractional) center of the frame, S is the 
   % scale and TH is the orientation (in radians).
+  
+  % sort matches, so that points of img1 are in order
+  sorted_matches = sortrows(matches',1)';
   xt     = T( i, 1 );
   yt     = T( i, 2 );
-  xCntr  = f2(1, matches(2, i));
-  yCntr  = f2(2, matches(2, i));
-  radius = f2(3, matches(2, i));
+  xCntr  = f2(1, sorted_matches(2, i));
+  yCntr  = f2(2, sorted_matches(2, i));
+  radius = f2(3, sorted_matches(2, i));
   
   dist   = sqrt( (xt - xCntr)^2 + (yt - yCntr)^2 );
   
