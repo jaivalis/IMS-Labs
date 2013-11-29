58237129c829d393244b6a3cfdc12df17690c8c0
jaivalis <johnaivalis@gmail.com>
2013-11-29 14:41:29 +0100

    wip

--- Assignment4/src/Assignment4.m
+++ Assignment4/src/Assignment4.m
@@ -1,17 +1,32 @@
 image_count = 6;
-for i=1:image_count,
-  path = strcat('img',num2str(i),'.pgm');
+
+for i=1:image_count,  
+  path = strcat('../img',num2str(i),'.pgm');  
+  img = imread(path);
+  
   if size(img, 3) ~= 3,
-    grayImg = ;
+    grayImg = single( img );
   else
-    grayImg = double(rgb2gray(img));
+    grayImg = single(rgb2gray( img ));
   end
-  img_size = size(img); 
-  img(i, :, :) = single(rgb2gray(imread(path)));
-end
-% figure;
-% imshow(img1);
-for i=1:image_count,
-  [frames(i), desc(i)] = vl_sift(img(i));
+  img_size = size(grayImg);
+  SiftSingle.grayImg = grayImg;
+  I(i) = SiftSingle;
 end
 
+% figure;
+% imshow(img1);
+
+% frames = zeros(image_count, img_size(1), img_size(2));
+% desc = zeros(image_count, img_size(1), img_size(2));
+for i=1:image_count,
+%   img = single(reshape(I(i, :, :), img_size(1), img_size(2)));
+  img = I(i).grayImg;
+  [ frames, desc ]    = vl_sift( img );
+  
+  
+end
+
+for i = 1:image_count,
+  
+end
