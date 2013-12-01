b246d72ae0ffd140b27155e9b0367d9a73853fd0
TopSteely <tobias.stahl1@gmx.de>
2013-12-01 15:33:50 +0100

    built-in image transformation works, sometimes odd errors

--- Assignment4/src/Ransac.m
+++ Assignment4/src/Ransac.m
@@ -52,11 +52,12 @@ for n = 1:N, % repeat N times
   end
   
   % count inliers
-  inliers = countInliers( T, matches, f2 );
+  [inlierSet, inliers] = countInliers( T, matches, f2 );
   
   if inliers > bestInlierCount,
     bestInlierCount = inliers;
     bestSolution    = xx;
+    bestInlierSet   = inlierSet;
   end
   
   % plot transformation
@@ -84,49 +85,19 @@ for n = 1:N, % repeat N times
 end
 % end repeat
 
-% transfrmdImg = transformImage( img1, bestSolution );
-% figure;         imshowpair(img1, img2);
+% our solution:
+transfrmdImg = transformImage( img1, bestSolution );
+% figure;         imshowpair(img1, transfrmdImg, 'montage');
+
+% built-in image transformation solution:
+% reshape bestSolution
+Sol_reshaped = [bestSolution(1), bestSolution(2) 0;...
+                bestSolution(3), bestSolution(4) 0;...
+                bestSolution(5), bestSolution(6) 1];
+              
+form    = maketform('affine', Sol_reshaped);
+builtin = imtransform(img2, form);
+
+figure;         imshowpair(img1, builtin, 'montage');      
 
 end
-%     random_matches = matches(P);
-%     random_frame1 = frame1(random_matches(:, 1));
-%     random_frame2 = frame2(random_matches(:, 2));
-%     % construct matrix A and vector b
-%     % matches has shape [X,Y - Value, matches_count]
-%     X = random_frame1(1, :);
-%     Y = random_frame1(2, :);
-%     X_prime = random_frame2(1, :);
-%     Y_prime = random_frame2(2, :);
-%     A = zeros(2 * P, 6);
-%     b = zeros(2 * P, 1);
-%     for j = 1:P,
-%       % first value of frame is x-value
-%       % second value of frame is y-value
-%       desc_frame1 = matches(j,1);
-%       desc_frame2 = matches(j,2);
-%       x = frame1(1, desc_frame1);
-%       y = frame1(2, desc_frame1);
-%       x_prime = frame1(1, desc_frame2);
-%       y_prime = frame1(2, desc_frame2);
-%       A(2 * (j-1) + 1, 1) = x;
-%       A(2 * (j-1) + 1, 2) = y;
-%       A(2 * (j-1) + 1, 3) = 0;
-%       A(2 * (j-1) + 1, 4) = 0;
-%       A(2 * (j-1) + 1, 5) = 1;
-%       A(2 * (j-1) + 1, 6) = 0;
-%       A(2 * j, 1) = 0;
-%       A(2 * j, 2) = 0;
-%       A(2 * j, 3) = x;
-%       A(2 * j, 4) = y;
-%       A(2 * j, 5) = 0;
-%       A(2 * j, 6) = 1;
-%       b((j-1) + 1, 1) = x_prime;
-%       b(2 * j, 1) = y_prime;
-%     % possible without loop?
-%     end
-%     x = pinv(A) * b;
-%     figure;
-%     imshowpair(I(imgnmb1).grayImg, I(imgnmb2).grayImg);
-%     hold on
-%     
-%     hold off
--- Assignment4/src/countInliers.m
+++ Assignment4/src/countInliers.m
@@ -1,4 +1,4 @@
-function inliers = countInliers( T, matches, f2 )
+function [inlierSet, inliers] = countInliers( T, matches, f2 )
 %COUNTINLIERS counts the number of inliers
 %   Inliers are defined as the number of transformed points from image T 
 % that lie within a 10 pixel radius from their pair in img.
@@ -10,6 +10,7 @@ function inliers = countInliers( T, matches, f2 )
 % 
 % OUTPUT
 % - inliers: scalar, count of inliers
+% - inlierSet: set of inliers
 
 inliers = 0;
 
@@ -17,18 +18,19 @@ for i = 1:T,
   % [X;Y;S;TH], where X,Y is the (fractional) center of the frame, S is the 
   % scale and TH is the orientation (in radians).
   
-  % sort matches, so that points of img1 are in order
-  sorted_matches = sortrows(matches',1)';
+  % matches are already sorted
+%   sorted_matches = sortrows(matches',1)';
   xt     = T( i, 1 );
   yt     = T( i, 2 );
-  xCntr  = f2(1, sorted_matches(2, i));
-  yCntr  = f2(2, sorted_matches(2, i));
-  radius = f2(3, sorted_matches(2, i));
+  xCntr  = f2(1, matches(2, i));
+  yCntr  = f2(2, matches(2, i));
+  radius = f2(3, matches(2, i));
   
   dist   = sqrt( (xt - xCntr)^2 + (yt - yCntr)^2 );
   
   if dist - radius < 10,
     inliers = inliers + 1;
+    inlierSet(inliers, :) = [xCntr, yCntr];
   end
 end
 
