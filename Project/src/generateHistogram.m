38ad0caa2d0f3358de20d2f940acb05047be9566
TopSteely <tobias.stahl1@gmx.de>
2013-12-09 15:49:31 +0100

    hist

--- Project/src/generateHistogram.m
+++ Project/src/generateHistogram.m
@@ -5,7 +5,15 @@ function generateHistogram( b_o_w )
 % INPUT
 % - b_o_w: Bag of words
 
-
-
+  pCount = size(b_o_w, 1);
+  catCount = 4;
+  imgs_per_cat = pCount / catCount;
+  figure;
+  
+  for p=1:pCount,
+    subplot( catCount, imgs_per_cat, p);
+    hist( b_o_w(p, : ) );
+  end
+  
 end
 
