490bdce52d447ac9ab443197196bb33d4d9e9e55
TopSteely <tobias.stahl1@gmx.de>
2013-12-09 17:54:31 +0100

    gH returns histograms

--- Project/src/generateHistogram.m
+++ Project/src/generateHistogram.m
@@ -1,21 +1,29 @@
-function generateHistogram( images )
+function histograms = generateHistogram( images , surpressPlots)
 %GENERATEHISTOGRAM Visualization of the bag of words
 %   Makes use of MATLAB 'hist' function to visualize a given bag of words
 %
 % INPUT
-% - b_o_w: Bag of words
+% - images: Matrix containing instances of class image
+% - surpressPlots: boolean flag for plotting
+%
+% OUTPUT
+% - histograms: Matrix containing histogram of each image
 
   imageCount = length( images );
   catCount   = 4;
   imgs_per_cat = imageCount / catCount;
-  figure;
-  hold on;
+  histograms = zeros(imageCount, 400);
+  if ~surpressPlots
+    figure;
+  end
   for i = 1:imageCount,
     image = images(i);
-    subplot( catCount, imgs_per_cat, i);
-    hist( image.bagOfWords );
+    histograms(i, :) = hist( image.bagOfWords , 400 );
+    if ~surpressPlots
+      subplot( catCount, imgs_per_cat, i);
+      hist( image.bagOfWords );
+    end
   end
-  hold off;
 
 end
 
