def060ab87081e45143b00858c89292cb786c7ad
jaivalis <johnaivalis@gmail.com>
2013-12-09 16:34:18 +0100

    added SiftImage

---
+++ Project/src/SiftImage.m
@@ -0,0 +1,41 @@
+classdef SiftImage
+  %SIFTIMAGE Summary of this class goes here
+  %   Detailed explanation goes here
+  
+  properties
+    img
+    grayImg
+    sampleDescs
+    allDescs
+    imgSize
+    classLabel
+    bagOfWords % vector that contains one word per descriptor of this image
+  end
+  
+  methods
+    % default constructor
+    function obj = SiftImage( img, classLabel, descCount )
+      obj.img        = img;
+      obj.grayImg    = zeros(size(img));
+      obj.classLabel = classLabel;
+      
+      if size(img, 3) ~= 3,
+        obj.grayImg = single( img );
+      else
+        obj.grayImg = single(rgb2gray( img ));
+      end
+      obj.imgSize = size( obj.grayImg );
+      
+      % generate sampleDescs, choose 'descCount' descriptors to keep (randomly)
+      [~, obj.allDescs] = vl_dsift( obj.grayImg );
+      rand         = randi(size(obj.allDescs(:, :), 2), 1, descCount);   
+      
+      obj.allDescs    = single( obj.allDescs );
+      obj.sampleDescs = obj.allDescs(:, rand);
+      obj.bagOfWords  = zeros( 1, size(obj.sampleDescs, 2) );
+    end
+      
+  end
+  
+end
+
