85655bf2c045af698a76ead442d6bc0c08df7a75
jaivalis <johnaivalis@gmail.com>
2013-12-09 14:41:17 +0100

    added visVocab

---
+++ Project/src/generateHistogram.m
@@ -0,0 +1,11 @@
+function generateHistogram( b_o_w )
+%GENERATEHISTOGRAM Visualization of the bag of words
+%   Makes use of MATLAB 'hist' function to visualize a given bag of words
+%
+% INPUT
+% - b_o_w: Bag of words
+
+
+
+end
+
